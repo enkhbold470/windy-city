05694de7f5f188aafbfde712a573749f0d64cbf2
danielesiegel <danieledisonsiegel@gmail.com>
2024-11-16 13:03:27 -0500

    commit test

--- frontend/components/3DWidget.tsx
+++ frontend/components/3DWidget.tsx
@@ -2,7 +2,8 @@
 
 import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
 
 const ThreeHeatmap = ({
   xValues,
