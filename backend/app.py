e8612f7019ce12a0c039a9bbbb4af729a602470c
Nikita Agrawal <nagra@dhcp-10-29-161-169.dyn.MIT.EDU>
2024-11-16 01:22:45 -0500

    Add process.py and update .gitignore, app.py, and requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import process
+from flask import Flask, jsonify
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_squared_error, r2_score
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    output = process.get_crop_feasibility()
+    return output
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001)
